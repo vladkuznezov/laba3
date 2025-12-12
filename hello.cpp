408b3a6d3439f22179cb26e15ae9102a8658e4d2
vladkuznezov <145802422+vladkuznezov@users.noreply.github.com>
2025-12-12 14:43:13 +0300

    Create hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Привет! Это тест CI/CD конвейера." << std::endl;
+    std::cout << "Программа успешно собрана автоматически!" << std::endl;
+    return 0;
+}
